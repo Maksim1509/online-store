3b36de90fc7f51aad8805dc38c2c4590f1effc99
Maksim1509 <maaa23@mail.ru>
2022-12-23 22:00:54 +0400

    feat: implement Product component logic

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,9 +1,16 @@
-const Product = () => {
-  // TO DO
+import { IProduct } from '../../types';
+import './product.css';
+
+const Product = (data: IProduct) => {
   return (
-    <div>
-      <h3>Product Name here</h3>
-    </div>
+    <section className='product'>
+      <h3>Category: {data.category}</h3>
+      <h3>{data.title}</h3>
+      <span>Price: {data.price}</span>
+      <p>{data.description}</p>
+      <span>Stok: {data.stock}</span>
+      <img className='product__img' src={data.images[0]} alt={data.title} />
+    </section>
   );
 };
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,17 @@
+import Product from '../components/Product/Product';
+import data from '../data/data.json';
+import { IProduct } from '../types';
+
 const MainPage = () => {
+  const renderProducts = (data: IProduct[]) =>
+    data.map((item) => <Product key={item.id} {...item} />);
+
   return (
     <>
       <h1>Main</h1>
+      <section className='main__products'>
+        {renderProducts(data.products)}
+      </section>
     </>
   );
 };
