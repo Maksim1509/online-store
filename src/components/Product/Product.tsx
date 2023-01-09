5510fe8f8101af31b49889754ec0c98d518e423b
Maksim1509 <maaa23@mail.ru>
2023-01-09 18:43:58 +0400

    refactor: попправил переход на продукт

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Context } from '../../context/Context';
 import { IProduct } from '../../types';
 import './product.css';
 
 const Product = (data: IProduct) => {
+  const navigate = useNavigate();
   const { cart, updateCart } = useContext(Context);
   const addToCardHendler =
     (data: IProduct) => (event: { preventDefault: () => void }) => {
@@ -17,20 +18,22 @@ const Product = (data: IProduct) => {
       updateCart([...cart, data]);
     };
 
+  const linkToProductHendler = (id: number) => () => {
+    navigate(`product/${id}`);
+  };
+
   return (
-    <Link to={`/product/${data.id}`} target='_blank'>
-      <section className='product'>
-        <h3>Category: {data.category}</h3>
-        <h3>{data.title}</h3>
-        <span>Price: {data.price}</span>
-        <p>{data.description}</p>
-        <span>Stok: {data.stock}</span>
-        <img className='product__img' src={data.images[0]} alt={data.title} />
-        <button className='addBtn' onClick={addToCardHendler(data)}>
-          Add to cart
-        </button>
-      </section>
-    </Link>
+    <section className='product' onClick={linkToProductHendler(data.id)}>
+      <h3>Category: {data.category}</h3>
+      <h3>{data.title}</h3>
+      <span>Price: {data.price}</span>
+      <p>{data.description}</p>
+      <span>Stok: {data.stock}</span>
+      <img className='product__img' src={data.images[0]} alt={data.title} />
+      <button className='addBtn' onClick={addToCardHendler(data)}>
+        Add to cart
+      </button>
+    </section>
   );
 };
 
